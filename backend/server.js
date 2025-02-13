3ccc9fb83f3e504740331af415ea184d3e974a74
FGacheru <gacheruf12@gmail.com>
2025-02-14 00:51:31 +0300

    Create the CRUD APIs in the Backend

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-import productRoutes from "./routes/product.route.js"
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 const app = express();
 
 app.use(express.json()); //Middleware that allows us to accept json data in the req.body before the data is sent
-const PORT = 5000;
 
-app.use("/api/products", productRoutes)
+app.use("/api/products", productRoutes);
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   connectDB();
-  console.log(`Server started at http://localhost:${5000}`);
+  console.log("Server started at http://localhost:" + PORT);
 });
