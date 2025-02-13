1155a35dbd973bea40c8d01011e7385bc3f46220
FGacheru <gacheruf12@gmail.com>
2025-02-13 15:56:22 +0300

    Connect to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,56 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
 
+dotenv.config();
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Server is ready now")
-})
+app.use(express.json()); //Middleware that allows us to accept json data in the req.body before the data is sent
+const PORT = 5000;
 
-app.listen(5000, () => {
-    console.log("Server started at http://localhost:5000");
-    
-})
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.log("Error in fetching products");
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+});
+
+app.post("/api/products", async (req, res) => {
+  const product = req.body; //user will send this data
+
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please provide all the fields" });
+  }
+  const newProduct = new Product(product);
+  try {
+    await newProduct.save();
+    res.status(200).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.log("Error in create product", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
+
+
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: "Product deleted" });
+  } catch (error) {
+    console.log("Error in deleting product", error.message);
+    res.status(404).json({ success: false, message: "product not found" });
+  }
+});
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server started at http://localhost:${5000}`);
+});
