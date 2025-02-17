57dd57ffaa5ee956495aa8e7dc5419a8ebdb425f
FGacheru <gacheruf12@gmail.com>
2025-02-17 14:37:40 +0300

    Create add product UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,13 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
 import CreatePage from "./pages/CreatePage.jsx";
 import  NavBar from "./components/NavBar.jsx";
+import { useColorModeValue } from "@chakra-ui/react";
+
 
 
 function App() {
   return (
-    <Box minH={"100vh"}>
+    <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
       <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
